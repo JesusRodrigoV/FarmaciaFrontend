a5a513b17fbde568c6609dde721d7f4b1d667dd0
JesusRodrigoV <jaicelvelasco105@gmail.com>
2024-11-26 00:21:11 -0400

    Creacion de servicios angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { VentaService } from './services/venta/venta.service';
+import { UsuarioService } from './services/usuario/usuario.service';
+import { RecetaMedicaService } from './services/recetaMedica/receta-medica.service';
+import { ProveedorService } from './services/proveedor/proveedor.service';
+import { ProductoService } from './services/producto/producto.service';
+import { DetalleVentaService } from './services/detalleVenta/detalle-venta.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
-import { EstadosService } from './services/estados/estados.service';
-import { PaisesService } from './services/paises/paises.service';
-import { PersonaService } from './services/persona/persona.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { AfterViewInit } from '@angular/core';
 
@@ -19,7 +22,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
 
-  personaForm: UntypedFormGroup;
+  venta: UntypedFormGroup;
   paises: any;
   estados: any;
   personas: any;
@@ -32,9 +35,12 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   constructor(
     public fb: UntypedFormBuilder,
-    public estadosService: EstadosService,
-    public paisesService: PaisesService,
-    public personaService: PersonaService
+    public detalleVentaService: DetalleVentaService,
+    public productoService: ProductoService,
+    public proveedorService: ProveedorService,
+    public recetaMedicaService: RecetaMedicaService,
+    public usuarioService: UsuarioService,
+    public ventaService: VentaService
   ) {
 
   }
@@ -43,7 +49,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
   ngOnInit(): void {
 
-    this.personaForm = this.fb.group({
+    this.venta = this.fb.group({
       id: [''],
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
@@ -52,72 +58,9 @@ export class AppComponent implements OnInit, AfterViewInit {
       estado: ['', Validators.required],
     });
 
-    this.paisesService.getAllPaises().subscribe(resp => {
-      this.paises = resp;
-    },
-      error => { console.error(error) }
-    );
 
-    this.personaService.getAllPersonas().subscribe(resp => {
-      this.personas = resp;
-      this.setDataAndPagination();
-    },
-      error => { console.error(error) }
-    );
-
-    this.personaForm.get('pais').valueChanges.subscribe(value => {
-      this.estadosService.getAllEstadosByPais(value.id).subscribe(resp => {
-        this.estados = resp;
-      },
-        error => { console.error(error) }
-      );
-    })
   }
 
-  /**
-   * Metodo que llama el boton de guardar. Enviamos la peticion la servicio, luego reseteamos el formulario, filtramos
-   * y reseteamos la paginacion.
-   */
-  guardar(): void {
-    this.personaService.savePersona(this.personaForm.value).subscribe(resp => {
-      this.personaForm.reset();
-      this.personaForm.setErrors(null);
-      this.personas=this.personas.filter(persona=> resp.id!==persona.id);
-      this.personas.push(resp);
-      this.setDataAndPagination();
-    },
-      error => { console.error(error) }
-    )
-  }
-
-  /**
-   * Metodo que elimina una persona, luego reseteamos la paginacion.
-   * @param persona parametro donde se indica la persona a eliminar.
-   */
-  eliminar(persona){
-    this.personaService.deletePersona(persona.id).subscribe(resp=>{
-      if(resp){
-        this.personas.pop(persona);
-        this.setDataAndPagination();
-      }
-    })
-  }
-
-  /**
-   * Seteamos los datos en el formulario con la persona que vamos a editar.
-   * @param persona parametro donde se indica la persona a eliminar.
-   */
-  editar(persona){
-    this.personaForm.setValue({
-      id:persona.id,
-      nombre: persona.nombre ,
-      apellido: persona.apellido ,
-      edad: persona.edad,
-      pais: persona.pais,
-      estado: persona.estado,
-    });
-    this.panelOpenState = !this.panelOpenState;
-  }
 
   setDataAndPagination(){
     this.dataSource = new MatTableDataSource(this.personas);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,34 +10,37 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatSortModule} from '@angular/material/sort'
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSortModule } from '@angular/material/sort'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
-@NgModule({ declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        MatTableModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatPaginatorModule,
-        MatExpansionModule,
-        MatSortModule,
-        MatToolbarModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatSortModule,
+    MatToolbarModule], providers: [provideHttpClient(withInterceptorsFromDi())]
+})
 export class AppModule { }
