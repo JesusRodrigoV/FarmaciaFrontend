e49914cc17ed5643e6c6e58c8f0f73bb6fc1f214
JesusRodrigoV <jaicelvelasco105@gmail.com>
2024-11-26 20:07:34 -0400

    Migrando a angular 19

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppRoutingModule } from '../app-routing.module';
 import { LoginComponent } from '../login/login.component';
 import { ReporteComponent } from '../reporte/reporte.component';
 import { InventarioComponent } from '../inventario/inventario.component';
@@ -8,7 +7,7 @@ import { InventarioComponent } from '../inventario/inventario.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, AppRoutingModule, LoginComponent, NavbarComponent, ReporteComponent, InventarioComponent],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, ReporteComponent, InventarioComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
